fd6660baf7d0b2432ce2d59f967bb2532fb0a2b6
david.mugisha <david.mugisha@accenture.com>
2018-06-29 10:36:05 +0200

    Adding prompt from the user

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,24 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-	    Game garros = new Game("Nadal", "Djokovic");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter name of the first player (Nadal):");
+        String playerOne = br.readLine();
+        if("".equals(playerOne)){
+            playerOne = "Nadal";
+        }
+        System.out.print("Enter name of the first player(Djokovic):");
+        String playerTwo = br.readLine();
+        if("".equals(playerTwo)){
+            playerTwo = "Djokovic";
+        }
+	    Game garros = new Game(playerOne, playerTwo);
 	    garros.simulateGame(100);
     }
 }
